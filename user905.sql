ce01ee33855be9c067ab8b1c42bf88371cdbdc99
SreelakshmiSajeevan <54982096+SreelakshmiSajeevan@users.noreply.github.com>
2022-08-19 11:01:26 +0530

    Create user905.sql

---
+++ user905.sql
@@ -0,0 +1,8 @@
+create table user905(
+id int not null auto_increment,
+name varchar(100) not null,
+password varchar(100) not null,
+email varchar(100) not null,
+country varchar(100) not null,
+primary key(id)
+);
